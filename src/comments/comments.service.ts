c9d3d0ce20fb04b30187a230fa511ee3c3ef46fd
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-05-04 08:39:20 +0200

    Finished comments service + new DB migration

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto'
+import { UpdateCommentDto } from './dto/update-comment.dto'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+    constructor(private prisma: PrismaService) {}
+
+    async findAll(limit?: number) {
+        const comments = await this.prisma.comment.findMany({
+            ...(limit && { take: limit }) 
+        })
+        return comments
+    }
+
+    async findOne(id: string) {
+        const comment = await this.prisma.comment.findUnique({ where: { id } })
+        if (!comment) throw new NotFoundException(`Comment with id ${id} not found`)
+        return comment
+    }
+
+    async create(comment: CreateCommentDto) {
+        const newComment = await this.prisma.comment.create({ data: comment })
+        return newComment
+    }
+
+    async update(id: string, comment: UpdateCommentDto) {
+        const oldComment = await this.prisma.comment.update({
+            where: { id },
+            data: comment
+        })
+        if (!oldComment) throw new NotFoundException(`Comment with id ${id} not found`)
+        return oldComment
+    }
+
+    async remove(id: string) {
+        const comment = await this.prisma.comment.delete({ where: { id } })
+        if (!comment) throw new NotFoundException(`Comment with id ${id} not found`)
+    }
+}
