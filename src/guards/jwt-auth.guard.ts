517e2586c9eec8978465ce1a81507d45c0ac700e
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-05-07 20:38:01 +0200

    Added a JwtAuthGuard guard for authorization

---
+++ src/guards/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, ExecutionContext, CanActivate, UnauthorizedException } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { Request } from 'express'
+import { JwtService } from '../auth/jwt/jwt.service'
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+    canActivate(
+        ctx: ExecutionContext
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const req = ctx.switchToHttp().getRequest<Request>()
+
+        const token = req.headers['authorization']?.split(' ')[1]
+        if (!token) throw new UnauthorizedException('No token provided')
+            
+        const decoded = this.jwtService.verify(token)
+        if (!decoded) throw new UnauthorizedException('Invalid token provided')
+        req['user'] = decoded
+
+        return true
+    }
+}
